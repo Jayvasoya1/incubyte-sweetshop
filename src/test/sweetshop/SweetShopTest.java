a51cdbe2a1e6d4878d4a2380cc2413cc4ddce7f0
Jay_Vasoya <jayvasoya02@gmail.com>
2025-07-16 19:39:57 +0530

    test: add deleteSweet test with SweetNotFoundException check

--- src/test/sweetshop/SweetShopTest.java
+++ src/test/sweetshop/SweetShopTest.java
@@ -1,10 +1,12 @@
 package test.sweetshop;
 
 import main.sweetshop.SweetShop;
+import main.sweetshop.exception.SweetNotFoundException;
 import main.sweetshop.Sweet;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 public class SweetShopTest {
@@ -21,6 +23,17 @@ public class SweetShopTest {
         // try to find it by ID and check the name
         Sweet result = shop.findById(1001);
         assertEquals("Kaju Katli", result.getName());
+    }
+
+    @Test
+    void testDeleteSweet() {
+        // add a sweet and delete it
+        SweetShop shop = new SweetShop();
+        shop.addSweet(new Sweet(1001, "Kaju Katli", "Nut-Based", 50, 10));
+        shop.deleteSweet(1001);
+
+        // try to find it now, should throw an exception
+        assertThrows(SweetNotFoundException.class, () -> shop.findById(1001));
     }
    
     
